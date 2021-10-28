2e77ad12fd977b4f4e1515f3407a76b4ae0253a7
RaulDevilish <82582091+RaulDevilish@users.noreply.github.com>
2021-10-28 21:56:32 +0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+let burg = document.querySelector('.burg')
+let body = document.querySelector('body')
+let closebtn = document.querySelector('.closebtn')
+
+burg.onclick = () => {
+    body.style.overflowY = 'hidden'
+}
